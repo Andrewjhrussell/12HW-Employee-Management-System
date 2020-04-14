07a5e57fdc5463ff47704586a19ab19ea6b47c19
Andrewjhrussell <andrewjhrussell@gmail.com>
2020-04-14 18:29:20 -0400

    update

--- MySql.js
+++ MySql.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer")
+const table = require('console.table');
 var connection = mysql.createConnection({
   host: "localhost",
     port:3306,
@@ -18,41 +19,88 @@ connection.connect(function(err) {
   });
 
 
+function start(){
+  inquirer.prompt([{
+    type:'list',
+    name: 'whatToDo',
+    message: 'What would you like to do?',
+    choices:['View Employees','Add Employee','View Roles', 'quite']
+  }]).then(what=>{
+    if(what.whatToDo === 'View Employees') viewEmp()
+    else if(what.whatToDo === 'Add Employee') createEmployee()
+    else if(what.whatToDo === 'View Roles') viewRole()
+    else process.exit()
+  })
+}
+  
 function createEmployee(){
- var questions =[
-     {
-         type:"input",
-         name:"ILLUMINITTI",
-         message:"What is the department name?"
-     },
-     {
-        type:"input",
-        first_name:"Sam",
-        message:"What is the Employee first name?"
-    },
-    {
-        type:"input",
-        Last_name:"Jefferson",
-        message:"What is the Employee last name?"
-    }
- ]
- inquirer.prompt(questions).then(function(userResponse){
-     console.log(userResponse)
-     connection.query("INSERT INTO EMPLOYEE(FIRST_Name, Last_Name, Role_ID, Managers_ID) VALUES (Sam, Jefferson, 1353, 9929)",[userResponse.firstName, userResponse.lastName, userResponse.RoleID, userResponse.ManagersID],
-        function (err, res) {
-            if(err) throw err
-            console.log(res)
-            start()
-        }
-     )
+   
+    connection.query('SELECT * FROM ROLES',(err, res)=>{
+        //console.log(res)
+        var rolesList = res.map(role=>{
+            return {
+                value: role.id,
+                name: role.EMPOLYEE_TITLE
+            }
+        })
+        connection.query('SELECT * FROM employees', function(err, res){
+          if (err) throw err
+          var managerList = res.map(emp=>{
+            return {
+              value: emp.id,
+              name: `${emp.FIRST_NAME} ${emp.LAST_NAME}`
+            }
+          })
+        inquirer.prompt(
+           [
+               {
+                   type:"input",
+                   name:"FIRST_NAME",
+                   message:"What is the first name?"
+               },
+               {
+                  type:"input",
+                  name:"LAST_NAME",
+                  message:"What is the employees last name?"
+              },
+              {
+                  type:"list",
+                  message:"What is the employees role?",
+                  name: 'ROLE_ID',
+                  choices:rolesList
+              },
+              {
+                type:"list",
+                message:"Who is the employees manager?",
+                name: 'MANAGER_ID',
+                choices:managerList
+            }
+            ]
+        ).then(function(userResponse){
+            console.log(userResponse)
+            connection.query("INSERT INTO employees SET ?",userResponse,
+               function (err, res) {
+                   if(err) throw err
+                   start()
+               }
+            )
+        })
+    })
+  })
+}
+
+function viewEmp() {
+     connection.query('SELECT FIRST_NAME, LAST_NAME, ROLE_ID, MANAGER_ID FROM employees', function(err, res){
+        if(err) throw err;
+        console.table(res);
+        start()
+    })
+}
+function viewRole() {
+  connection.query('SELECT * FROM ROLES', function(err, res){
+     if(err) throw err;
+     console.table(res);
+     start()
  })
 }
 
-function start() {
-     connection.query('SELECT FIRST_NAME FROM employees', function(err, res){
-        if(err) throw err;
-        console.log(res, '<====')
-        console.table(res)
-        createEmployee()
-    })
-}
